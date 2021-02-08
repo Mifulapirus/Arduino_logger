1e5829028d3f56bca6b0be19a29bb9b5e1a3789f
Ángel <angel@tupperbot.com>
2021-02-08 19:02:16 +0100

    updated debuging options and version

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,12 +1,12 @@
 #include <logger.h>
 
-
 #define DBG_OUTPUT_PORT Serial
 #define VERSION 0.1
 #define LOG_TO_FILE false
 #define LOG_FILE "/log.txt"
 #define CURRENT_LOG_FILE "/current_log.txt"
 
+bool DEBUG = true;
 bool initFS() {
   if (SPIFFS.begin()) return true;
   else return false;
@@ -21,36 +21,43 @@ void printLogPaths(){
   else DBG_OUTPUT_PORT.println("Logging to file is Disabled");
 }
 
+/***************************
+ * Serial logging helper with appropriate dating. It could also log to files and perform some other logging tasks
+ * @param data String to be logged. Current date will be added automatically
+ * @return void
+ ***************************/
 bool logger(String data){
-  //Print data via Serial port
-  DBG_OUTPUT_PORT.println(String(millis()) + "  " + data);
-  
-  if (LOG_TO_FILE) {
-    //Open log files
-    File logFile = SPIFFS.open(LOG_FILE, "a");
-    File currentLogFile = SPIFFS.open(CURRENT_LOG_FILE, "a");
-    if (!logFile || !currentLogFile) {
-      DBG_OUTPUT_PORT.println("Error opening log files");
-      return false;
-    }
+ if(DEBUG){
+    //Print data via Serial port
+    DBG_OUTPUT_PORT.println(String(millis()) + "  " + data);
+    
+    if (LOG_TO_FILE) {
+      //Open log files
+      File logFile = SPIFFS.open(LOG_FILE, "a");
+      File currentLogFile = SPIFFS.open(CURRENT_LOG_FILE, "a");
+      if (!logFile || !currentLogFile) {
+        DBG_OUTPUT_PORT.println("Error opening log files");
+        return false;
+      }
 
-    int bytesWritten = logFile.println(String(millis()) + "  " + data);
-    if (bytesWritten <= 0) {
-      DBG_OUTPUT_PORT.println("Log File write failed");
-      return false;
-    }
-    logFile.close();
+      int bytesWritten = logFile.println(String(millis()) + "  " + data);
+      if (bytesWritten <= 0) {
+        DBG_OUTPUT_PORT.println("Log File write failed");
+        return false;
+      }
+      logFile.close();
 
-    bytesWritten = currentLogFile.println(String(millis()) + "  " + data);
-    if (bytesWritten <= 0) {
-      DBG_OUTPUT_PORT.println("CurrentLog File write failed");
-      return false;
+      bytesWritten = currentLogFile.println(String(millis()) + "  " + data);
+      if (bytesWritten <= 0) {
+        DBG_OUTPUT_PORT.println("CurrentLog File write failed");
+        return false;
+      }
+      currentLogFile.close();
+      return true;
     }
-    currentLogFile.close();
     return true;
   }
-  
-  return true;
+  else return true;
 }
 
 String clearCurrentLogFile() {
