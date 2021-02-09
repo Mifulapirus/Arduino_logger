4e0523f8f015ce59c824c94970edb75f1b78120e
Ángel <angel@tupperbot.com>
2021-02-09 11:51:24 +0100

    header

--- include/Logger.h
+++ include/Logger.h
@@ -1,17 +1,17 @@
 #ifndef LOGGER_H
-#define LOGGER_H
+    #define LOGGER_H
 
-#include <Arduino.h>
-#include <FS.h>
-#include <SPIFFS.h>
+    #include <Arduino.h>
+    #include <FS.h>
+    #include <SPIFFS.h>
 
-bool initFS();
-bool logger(String stuff);
-String clearCurrentLogFile();
-void printLogPaths();
-bool initLogger(bool debug);
+    bool LOG_TO_SERIAL;
+    bool LOG_TO_FILE;
 
-bool LOG_TO_SERIAL;
-bool LOG_TO_FILE;
+    bool initFS();
+    bool logger(String stuff);
+    String clearCurrentLogFile();
+    void printLogPaths();
+    bool initLogger(bool debug);
 
 #endif
